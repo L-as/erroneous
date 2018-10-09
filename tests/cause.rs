a5141b55d7143511c26b251b0630da2f827a6340
Las <las@protonmail.ch>
2018-10-09 14:16:02 +0200

    Add tests

---
+++ tests/cause.rs
@@ -0,0 +1,19 @@
+#[macro_use]
+extern crate derive_more;
+extern crate erroneous;
+
+use erroneous::Error;
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "A")]
+struct A;
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "B")]
+struct B(#[error(cause)] A);
+
+#[test]
+fn main() {
+	let b = B(A);
+	let _ = b.cause().unwrap();
+}
